ed7048da60c8d63552d4bd1d40cf3d013182d548
Misko Hevery <misko@hevery.com>
2011-03-20 15:23:55 +0100

    wip

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,25 +4,52 @@ function CalculatorController(){
 
 CalculatorController.prototype = {
   clear: function(){
-    this.value = 0;
-    this.display = "0.";
+    this.register = 0;
+    this.setValue(0);
+    this.updateDisplay();
+    this.operation = null;
+  },
+
+  setValue: function(value){
+    this.value = value;
     this.decimalPosition = null;
   },
 
-  updateDisplay: function(){
-    this.display = this.value.toString();
+  updateDisplay: function(value){
+    value = value == undefined ? this.value : value;
+    this.display = value.toString();
     if (!this.display.match(/\./)) {
       this.display += '.';
     }
     while(this.display.match(/\.(.*)/)[1].length + 1 < this.decimalPosition) {
       this.display += '0';
     }
+    return value;
   },
 
   key: function(key) {
     switch(key){
       case '.':
         this.decimalPosition = this.decimalPosition || 1;
+        break;
+      case '/':
+        if (this.operation && !this.operationApplied) {
+          this.setValue(this.register / this.value);
+          this.updateDisplay();
+          this.register = this.value;
+          this.operationApplied = true;
+        } else if (this.operation != key) {
+          this.register = this.value;
+          this.operation = '/';
+          this.setValue(0);
+          this.operationApplied = false;
+        }
+        break;
+      case '=':
+        if(this.operation) {
+          this.register = this.updateDisplay(this.register / this.value);
+          this.operationApplied = true;
+        }
         break;
     }
   },
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,22 +1,22 @@
-describe('CaluculatorController', function(){
+describe('CaluculatorController', function() {
   var calc;
 
-  beforeEach(function(){
+  beforeEach(function() {
     calc = new CalculatorController();
   });
 
-  it('should initialize with zero', function(){
+  it('should initialize with zero', function() {
     expect(calc.display).toEqual('0.');
   });
 
-  it('should process number keys', function(){
+  it('should process number keys', function() {
     calc.number(1);
     expect(calc.display).toEqual('1.');
     calc.number(2);
     expect(calc.display).toEqual('12.');
   });
 
-  it('should process number keys after decimal', function(){
+  it('should process number keys after decimal', function() {
     calc.key('.');
     calc.number(0);
     calc.number(0);
@@ -26,6 +26,39 @@ describe('CaluculatorController', function(){
     calc.number(2);
     calc.number(3);
     expect(calc.display).toEqual('0.000123');
+  });
+
+  describe('divide', function() {
+    iit('should divide numbers using divide', function() {
+      calc.number(2);
+      calc.number(5);
+      calc.number(6);
+      calc.key('/');
+      expect(calc.display).toEqual('256.');
+      calc.number(2);
+      expect(calc.display).toEqual('2.');
+      calc.key('/');
+      expect(calc.display).toEqual('128.');
+      calc.key('/');
+      expect(calc.display).toEqual('128.');
+      calc.key('/');
+      expect(calc.display).toEqual('128.');
+      calc.key('=');
+      expect(calc.display).toEqual('1.');
+    });
+
+    iit('should divide numbers using equals', function() {
+      calc.number(2);
+      calc.number(5);
+      calc.number(6);
+      calc.key('/');
+      calc.number(2);
+      calc.key('=');
+      expect(calc.display).toEqual('128.');
+      calc.key('=');
+      expect(calc.display).toEqual('64.');
+    });
+
   });
 
 });
